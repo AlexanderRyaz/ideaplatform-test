78586097f18df1c8ed20bb1e583a3123f4cb00dd
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2023-03-16 14:30:28 +0300

    fixed error

--- src/main/java/org/example/service/CalculateService.java
+++ src/main/java/org/example/service/CalculateService.java
@@ -5,8 +5,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
-import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,27 +13,35 @@ import java.util.List;
 
 public class CalculateService {
 
-    private static int OFFSET = 8;
-
     public void execute() {
-        JSONParser        parser           = new JSONParser();
-        DateTimeFormatter simpleDateFormat = DateTimeFormatter.ofPattern("d.M.y;H:m");
+        JSONParser parser = new JSONParser();
+        DateTimeFormatter simpleDateFormat = DateTimeFormatter.ofPattern("d.M.yy;H:m");
         try {
             List<Duration> durations = new ArrayList<>();
-            Object         obj       = parser.parse(new FileReader("tickets.json"));
-            JSONObject     jo        = (JSONObject) obj;
-            JSONArray      array     = (JSONArray) jo.get("tickets");
+            Object obj = parser.parse(new FileReader("tickets.json"));
+            JSONObject jo = (JSONObject) obj;
+            JSONArray array = (JSONArray) jo.get("tickets");
             for (int i = 0; i < array.size(); i++) {
                 jo = (JSONObject) array.get(i);
                 String departureDate = (String) jo.get("departure_date");
                 String departureTime = (String) jo.get("departure_time");
-                String arrivalDate   = (String) jo.get("arrival_date");
-                String arrivalTime   = (String) jo.get("arrival_time");
+                String arrivalDate = (String) jo.get("arrival_date");
+                String arrivalTime = (String) jo.get("arrival_time");
 
                 LocalDateTime departureDateTime = LocalDateTime.parse(departureDate + ";" + departureTime, simpleDateFormat);
-                LocalDateTime arrivalDateTime   = LocalDateTime.parse(arrivalDate + ";" + arrivalTime, simpleDateFormat);
+                ZoneId zoneIdVdv = ZoneId.of("Asia/Vladivostok");
+                LocalDateTime arrivalDateTime = LocalDateTime.parse(arrivalDate + ";" + arrivalTime, simpleDateFormat);
+                ZoneId zoneIdTlv = ZoneId.of("Asia/Jerusalem");
+                ZonedDateTime zonedDateTimeVdv = ZonedDateTime.of(departureDateTime, zoneIdVdv);
+                ZonedDateTime zonedDateTimeTlv = ZonedDateTime.of(arrivalDateTime, zoneIdTlv);
 
-                Duration duration = Duration.between(departureDateTime, arrivalDateTime.plusHours(OFFSET));
+                Instant instantVdv = zonedDateTimeVdv.toInstant();
+                Instant instantTlv = zonedDateTimeTlv.toInstant();
+
+                ZonedDateTime zdtUtcVdv = instantVdv.atZone(ZoneId.of("Etc/UTC"));
+                ZonedDateTime zdtUtcTlv = instantTlv.atZone(ZoneId.of("Etc/UTC"));
+
+                Duration duration = Duration.between(zdtUtcVdv, zdtUtcTlv);
                 durations.add(duration);
             }
 
@@ -43,16 +50,16 @@ public class CalculateService {
                     .average()
                     .orElseThrow(() -> new RuntimeException("Невозможно рассчитать среднее время полета."));
 
-            int    differenceInHours   = (int) (averageDuration / 3600) % 24;
-            int    differenceInMinutes = (int) (averageDuration / 60) % 60;
+            int differenceInHours = (int) (averageDuration / 3600) % 24;
+            int differenceInMinutes = (int) (averageDuration / 60) % 60;
 
-                System.out.printf
-                        ("Среднее время полета между Владивостоком и Тель-Авивом %d часов и %d минут(ы).\n"
-                                , differenceInHours, differenceInMinutes);
+            System.out.printf
+                    ("Среднее время полета между Владивостоком и Тель-Авивом %d часов и %d минут(ы).\n"
+                            , differenceInHours, differenceInMinutes);
 
-            Duration percentile        = getPercentile(durations);
-            int      percentileHours   = (int) (percentile.getSeconds() / 3600.0) % 24;
-            int      percentileMinutes = (int) (percentile.getSeconds() / 60.0) % 60;
+            Duration percentile = getPercentile(durations);
+            int percentileHours = (int) (percentile.getSeconds() / 3600.0) % 24;
+            int percentileMinutes = (int) (percentile.getSeconds() / 60.0) % 60;
 
             System.out.printf
                     ("90-й процентиль времени полета между городами Владивосток и Тель-Авив %d часов и %d минут(ы)."
